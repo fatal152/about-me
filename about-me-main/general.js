255340d44f4ca435d27743a927600cf64e71e980
fatal152 <84009390+fatal152@users.noreply.github.com>
2024-07-28 22:02:01 +0800

    Delete about-me-main directory

--- about-me-main/general.js
+++
@@ -1,8 +0,0 @@
-
-
-  const button = document.querySelector('button');
-  const content = document.getElementById('mam');
-
-  button.addEventListener('click', () => {
-    content.textContent = "The content has been changed!";
-  });
